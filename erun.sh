e6bc6f22a4dda3d7801a5073ca924cf59790c14f
hcc <hcc@hust.edu.cn>
2020-01-03 00:05:01 +1100

    initial_hcc

---
+++ erun.sh
@@ -0,0 +1,4 @@
+python tools/test.py \
+    --cfg experiments/mpii/hrnet/w32_256x256_adam_lr1e-3.yaml \
+    TEST.MODEL_FILE output/mpii/pose_hrnet/w32_256x256_adam_lr1e-3/model_best.pth
+    # models/pytorch/pose_mpii/pose_hrnet_w32_256x256.pth
---
+++ run.sh
@@ -0,0 +1,3 @@
+python tools/train.py \
+ --cfg experiments/mpii/hrnet/w32_256x256_adam_lr1e-3.yaml \
+    #--cfg experiments/coco/hrnet/w32_256x192_adam_lr1e-3.yaml \
